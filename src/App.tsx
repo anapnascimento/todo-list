eccab19afda52b55940c5dcced8676f6e5fbd9a4
Ana Paula Souza do Nascimento <anapaula.uab@gmail.com>
2023-01-26 21:53:37 -0300

    Estrutura e Ícones
    Criação da estrutura html e adição da biblioteca de ícones

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import './App.css';
+import { Plus } from 'phosphor-react';
 
 export function App() {
+  function addTask() {
+    console.log('Add Task!');
+  }
   return (
     <div className="App">
-      <div>Inicio</div>
+      <div className="wrapper">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <main>
+          <form>
+            <input type="text" placeholder="Add new task"></input>
+            <button type="submit" onClick={addTask}>
+              Add <Plus size="32" color="#547BAF"></Plus>
+            </button>
+          </form>
+          <div className="task-count">
+            <div>
+              <p>Tasks created</p>
+              <span>0</span>
+            </div>
+            <div>
+              <p>Tasks completed</p>
+              <span>0</span>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
