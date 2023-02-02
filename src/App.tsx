58efe5f6879606a7f9deee8aa348c67929232990
Ana Paula Souza do Nascimento <anapaula.uab@gmail.com>
2023-02-01 21:39:37 -0300

    Customização
    Customizando o todo list

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ export function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <header>
+        <header className="header">
           <h1>Todo List</h1>
         </header>
         <main>
-          <form>
+          <form className="task-form">
             <input type="text" placeholder="Add new task"></input>
             <button type="submit" onClick={addTask}>
-              Add <Plus size="32" color="#547BAF"></Plus>
+              Add <Plus size="24" color="#ffffff"></Plus>
             </button>
           </form>
           <div className="task-count">
